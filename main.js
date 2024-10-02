083edd8ea08e48f115d7f5a1c12d96ec925d9fd9
Liam Seck <liamseck@live.ie>
2024-10-02 15:59:51 +0100

    adding comments

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ import { updateAddress } from "./updateAddress.js";
 import { submitCheckout } from "./submitCheckout.js";
 
 export const options = {
-  vus: 20,
+  // K6 bills in VUh 
+  // (Maximum number of VUs x test execution duration in minutes) / 60 minutes = VUh
+  // E.g (10 VUs x 5 mins)/60 = 0.83 VUh
+  
+  // Number of Virtual Users
+  vus: 10,
+  // Duration 
   duration: '5m',
   cloud: {
     // Project: WPE WooCommerce Load Testing
@@ -22,9 +28,7 @@ export const options = {
       http_req_failed: ['rate<0.01'], // http errors should be less than 1%
       http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
     },
-
-
-  }
+  },
 };
 
 // used to store global variables
@@ -35,7 +39,7 @@ globalThis.pauseMin = 5;
 globalThis.pauseMax = 10;
 
 export default function main() {
-  // Executes requests defined in navigateHomepage() script
+  // Executes requests defined in navigateHomepage()
   navigateHomepage();
   // Executes requests defined in addToCart()
   addToCart();
